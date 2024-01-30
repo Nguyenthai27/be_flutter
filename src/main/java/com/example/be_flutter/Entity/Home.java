8b773086f5e3e82dc21996b639fc309a3090e5d0
Nguyenthai27 <ptejthaii@gmail.com>
2024-01-30 20:18:45 +0700

    commit

---
+++ src/main/java/com/example/be_flutter/Entity/Home.java
@@ -0,0 +1,13 @@
+package com.example.be_flutter.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Home {
+    private String name;
+    private String image;
+}
